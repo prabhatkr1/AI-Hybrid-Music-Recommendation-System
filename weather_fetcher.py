811f048a04fada2da51b60467f5f7af5b2c2ef86
PRABHAT KUMAR <pkgorai6644@gmail.com>
2026-03-30 17:37:18 +0530

    Update weather_fetcher.py

--- weather_fetcher.py
+++ weather_fetcher.py
@@ -1,12 +1,24 @@
-import requests, os
-from dotenv import load_dotenv
-
-load_dotenv()
-API_KEY = os.getenv("OPENWEATHERMAP_API_KEY")
+import requests
+import os
 
 def get_weather(city):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-    res = requests.get(url).json()
-    weather = res["weather"][0]["main"]
-    temp = res["main"]["temp"]
+    api_key = os.getenv("API_KEY")
+
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+    res = requests.get(url)
+    
+    # Agar API fail ho jaye
+    if res.status_code != 200:
+        return "Unknown", 0
+
+    data = res.json()
+
+    # Safe check
+    if "weather" not in data or "main" not in data:
+        return "Unknown", 0
+
+    weather = data["weather"][0]["main"]
+    temp = data["main"]["temp"]
+
     return weather, temp
